2ac35088053c4a5a5b0db0047b137f759637f384
John <baezjohn312@gmail.com>
2023-01-05 13:21:02 -0500

    Initial Commit

---
+++ Student Voice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Voice
+//
+//  Created by 8h on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
