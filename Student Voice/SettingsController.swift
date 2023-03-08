c07077e2bb549b1160b21415a012e415737f75b8
John <baezjohn312@gmail.com>
2023-03-08 14:03:49 -0500

    ok

---
+++ Student Voice/SettingsController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsController.swift
+//  Student Voice
+//
+//  Created by 8h on 3/8/23.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func privacy(_ sender: Any) {
+        
+    }
+    
+    @IBAction func security(_ sender: Any) {
+    }
+    
+     @IBAction func MyAccount(_ sender: Any) {
+     }
+     // MARK: - Navigation
+    @IBAction func Help(_ sender: Any) {
+    }
+    
+    @IBAction func about(_ sender: Any) {
+    }
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
