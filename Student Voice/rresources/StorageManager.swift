42e8135830ea624c03f7c70b29a1f3e8315624de
Christopher Dominguez <Neymar15216@gmail.com>
2023-04-28 14:23:44 -0400

    Update

---
+++ Student Voice/rresources/StorageManager.swift
@@ -0,0 +1,22 @@
+//
+//  StorangeManager.swift
+//  Student Voice
+//
+//  Created by 6f on 4/28/23.
+//
+
+import Firebase
+import FirebaseStorage
+
+
+public class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // Mark: - Public
+    
+
+    
+    
+    
+}
