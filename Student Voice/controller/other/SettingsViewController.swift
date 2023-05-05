a3d602a91ce8742625ef662e57d0294723c8283e
Christopher Dominguez <Neymar15216@gmail.com>
2023-05-05 13:34:10 -0400

    dub

---
+++ Student Voice/controller/other/SettingsViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SettingsViewController.swift
+//  Student Voice
+//
+//  Created by 6f on 5/4/23.
+//
+
+import UIKit
+
+struct SettingCellModel {
+    let title: String
+    let handler: (() -> Void)
+}
+
+/// View Controller to show user settings
+final class SettingsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero,
+                                    style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = . systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
+    
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    //    cell.textLabel?.text = ""
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Handle cell selection
+    }
+}
