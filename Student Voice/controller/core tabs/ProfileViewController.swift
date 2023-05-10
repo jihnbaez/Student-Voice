19e1490121a63673c20d66a55a4784d84f01e2c9
Christopher Dominguez <Neymar15216@gmail.com>
2023-05-10 13:13:18 -0400

    yyy

--- Student Voice/controller/core tabs/ProfileViewController.swift
+++ Student Voice/controller/core tabs/ProfileViewController.swift
@@ -12,11 +12,17 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
-
-        // Do any additional setup after loading the view.
+            configureNavigationBar()
     }
 
+    private func configureNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
+    }
     
+    @objc private func didTapSettingsButton() {
+        let vc = SettingsViewController()
+        vc.title = "Settings"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
--- Student Voice/controller/other/SettingsViewController.swift
+++ Student Voice/controller/other/SettingsViewController.swift
@@ -22,8 +22,13 @@ final class SettingsViewController: UIViewController {
         
         return tableView
     }()
+    
+    private var data = [[SettingCellModel]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureModels()
         view.backgroundColor = . systemBackground
         view.addSubview(tableView)
         tableView.delegate = self
@@ -36,26 +41,38 @@ final class SettingsViewController: UIViewController {
         tableView.frame = view.bounds
     }
 
+    private func configureModels() {
+        let section = [
+            SettingCellModel(title: "Log Out") { [weak self] in
+                self?.didTapLogOut()
+            }
+       ]
+        data.append(section)
+    }
+    
+    private func didTapLogOut() {
+         
+    }
     
 }
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return data[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    //    cell.textLabel?.text = ""
+        cell.textLabel?.text = data[indexPath.section][indexPath.row].title
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        // Handle cell selection
+        data[indexPath.section][indexPath.row].handler()
     }
 }
