bcf31d878039edafe17cfac02c406aa165dfd0b5
Christopher Dominguez <Neymar15216@gmail.com>
2023-05-17 13:24:51 -0400

    ttt

--- Student Voice/controller/other/EditProfileViewController.swift
+++ Student Voice/controller/other/EditProfileViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController, UITableViewDataSource {
+struct EditProfileFormModel {
+    let label: String
+    let placeholder: String
+    var value: String
+}
+
+final class EditProfileViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 1
     }
-    
-   
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -20,14 +24,58 @@ class EditProfileViewController: UIViewController, UITableViewDataSource {
         return tableView
     }()
     
+    private var models = [[EditProfileViewController]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureModels()
+        tableView.tableHeaderView = createTableHeaderView()
         tableView.dataSource = self
         view.addSubview(tableView)
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSave))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancel))
     }
+    
+    private func configureModels() {
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // MARK: - Tablebiew
+    
+    private func createTableHeaderView() -> UIView {
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height/4).integral)
+        let size = header.frame.height/1.5
+        let profilePhotoButton = UIButton(frame: CGRect(x: (view.frame.width-size)/2, y: (header.frame.height-size)/2, width: size, height: size))
+        header.addSubview(profilePhotoButton)
+        profilePhotoButton.layer.masksToBounds = true
+        profilePhotoButton.layer.cornerRadius = size/2.0
+        profilePhotoButton.tintColor = UIColor.blue
+        profilePhotoButton.addTarget(self, action: #selector(didTapChangeProfielPicture), for: .touchUpInside)
+        
+        profilePhotoButton.setBackgroundImage(UIImage(systemName: "person.circle"), for: .normal)
+        profilePhotoButton.layer.borderWidth = 1
+        profilePhotoButton.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        
+        return header
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+    
+    // Mark: - Action
     
     @objc private func didTapSave() {
         // Save info to database
